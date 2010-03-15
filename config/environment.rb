5cb9e8198aa4df8edf2b1f8d47b156de43ad763d
Josh Owens <joshua.owens@gmail.com>
2010-03-15 02:18:23 -0400

    Adding formtastic gem to the app.

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,8 @@ Rails::Initializer.run do |config|
   config.gem 'haml',                      :version => '>= 2.2.0'
   config.gem 'compass',                   :version => '>= 0.8.5',                             :source => 'http://gemcutter.org'
   config.gem 'grid-coordinates',          :version => '>= 1.0.3',   :lib => 'compass',        :source => 'http://gemcutter.org'
+  config.gem 'formtastic'
+  config.gem 'validation_reflection'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
