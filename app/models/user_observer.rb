f37432ffb4be7d3e17c79d25549be29b00972279
Josh Owens <joshua.owens@gmail.com>
2010-11-15 18:24:19 -0500

    Make sure we pass the user.id, not the whole user.

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,6 +1,6 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
-    UserMailer.deliver_welcome_email(user) unless (user.admin? && User.count == 1)
+    UserMailer.deliver_welcome_email(user.id) unless (user.admin? && User.count == 1)
   end
   
 end
