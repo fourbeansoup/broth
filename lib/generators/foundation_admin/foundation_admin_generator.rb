4be3580376f72b3fdfe70c4a67a95ace9c7aac86
Josh Owens <joshua.owens@gmail.com>
2011-02-12 22:52:17 -0500

    Fix up generators for admin areas a bit more

--- lib/generators/foundation_admin/foundation_admin_generator.rb
+++ lib/generators/foundation_admin/foundation_admin_generator.rb
@@ -7,6 +7,7 @@ class FoundationAdminGenerator < Rails::Generator::NamedBase
       m.directory("app/views/admin/#{plural_name}")
       m.template("controller.rb", "app/controllers/admin/#{plural_name}_controller.rb")
       m.template("views/index.html.haml", "app/views/admin/#{plural_name}/index.html.haml")
+      m.template("views/_form.html.haml", "app/views/admin/#{plural_name}/_form.html.haml", :assigns => {:column_names => columns})
       m.template("views/new.html.haml", "app/views/admin/#{plural_name}/new.html.haml", :assigns => {:column_names => columns})
       m.template("views/edit.html.haml", "app/views/admin/#{plural_name}/edit.html.haml", :assigns => {:column_names => columns})
     end
