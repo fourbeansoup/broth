c22ee81d41347a56879555ddc414ccf19ab4359c
Josh Owens <joshua.owens@gmail.com>
2011-03-04 15:35:28 -0500

    Removing old gem setup and an outdated plugin

--- vendor/gems/devise-1.0.9/lib/devise/version.rb
+++
@@ -1,3 +0,0 @@
-module Devise
-  VERSION = "1.0.9".freeze
-end
--- vendor/gems/resque_mailer-1.0.0/lib/resque_mailer.rb
+++
@@ -1,7 +0,0 @@
-require "resque_mailer/common"
-
-if defined?(Rails.version) && Rails.version.to_i >= 3
-  require "resque_mailer/rails3"
-else
-  require "resque_mailer/rails2"
-end
